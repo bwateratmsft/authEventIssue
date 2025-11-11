bcfd975abdd48b007e9af2bf902e1e133882d9cc
Brandon Waterloo [MSFT] <36966225+bwateratmsft@users.noreply.github.com>
2025-11-11 14:53:09 -0500

    Make web?

--- extension.js
+++ extension.js
@@ -5,14 +5,27 @@
 
 // @ts-check
 
-// eslint-disable-next-line @typescript-eslint/no-var-requires
 const vscode = require('vscode');
 
 function activate() {
     const outputChannel = vscode.window.createOutputChannel('Auth provider tests', { log: true });
 
     vscode.authentication.onDidChangeSessions(async (e) => {
-        outputChannel.debug(`Authentication sessions changed for provider: ${e.provider.id}`);
+        outputChannel.warn(`Authentication sessions changed for provider: ${e.provider.id}`);
+    });
+
+    vscode.commands.registerCommand('extension.getAccounts', async () => {
+        const accounts = await vscode.authentication.getAccounts('microsoft');
+        outputChannel.info(`Found ${accounts.length} accounts`);
+    });
+
+    vscode.commands.registerCommand('extension.getSessionSilent', async () => {
+        const session = await vscode.authentication.getSession('microsoft', [], { silent: true });
+        if (session) {
+            outputChannel.info(`Found session for account: ${session.account.label}`);
+        } else {
+            outputChannel.info('No session found');
+        }
     });
 
     vscode.commands.registerCommand('extension.signIn', async () => {
